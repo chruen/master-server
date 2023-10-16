b677b77f51f0f523fdfd839b929d9c061d6cf986
chruen <chruen@126.com>
2023-10-16 16:40:52 +0800

    Initial commit

---
+++ src/main/java/com/swjtu/robot/masterserver/MasterServerApplication.java
@@ -0,0 +1,13 @@
+package com.swjtu.robot.masterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swjtu/robot/masterserver/MasterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swjtu.robot.masterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
